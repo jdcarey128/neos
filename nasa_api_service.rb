1c22640aacb8a50f33d224bf019b94f21195187a
Joshua Carey <jdcarey128@gmail.com>
2020-11-18 11:56:11 -0700

    Change methods to class methods

--- nasa_api_service.rb
+++ nasa_api_service.rb
@@ -1,16 +1,16 @@
 require 'faraday'
 
 class NasaApiService
-  def get_neos(date)
+  def self.get_neos(date)
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
     response = conn.get('/neo/rest/v1/feed')
-    parse_data(response, date)
+    self.parse_data(response, date)
   end
 
-  def parse_data(response, date)
+  def self.parse_data(response, date)
     JSON.parse(response.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
   end
 
