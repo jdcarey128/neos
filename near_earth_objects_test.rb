8804334ff9eb01c7826cc6bc07bbc0561338b850
Joshua Carey <jdcarey128@gmail.com>
2020-11-18 11:50:36 -0700

    Write test for largest asteroid

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -5,8 +5,15 @@ require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
   def test_a_date_returns_a_list_of_neos
-    results = NearEarthObjects.new.find_neos_by_date('2019-03-30')
+    instance = NearEarthObjects.new('2019-03-30')
+    results = instance.find_neos_by_date
     # assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
     assert_equal '(2011 GE3)', results[:astroid_list][0][:name]
+  end
+
+  def test_largest_asteroid_returns_it
+    instance = NearEarthObjects.new('2019-03-30')
+    result = instance.largest_astroid_diameter
+    assert_equal 537, result
   end
 end
