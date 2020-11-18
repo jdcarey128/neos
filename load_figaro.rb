9361a8eddfae7819c1df696389a5ddbd872013d8
Joshua Carey <jdcarey128@gmail.com>
2020-11-18 11:32:50 -0700

    Move Figaro to new file

---
+++ load_figaro.rb
@@ -0,0 +1,5 @@
+require 'figaro'
+
+# Load ENV vars via Figaro
+Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
+Figaro.load
